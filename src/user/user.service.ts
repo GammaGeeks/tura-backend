bbf0d6c0ea63488154e56582702f972327769f5e
NGOGA Adolphe <adolphengoga@gmail.com>
2024-11-13 07:36:24 +0200

    fixing config

---
+++ src/user/user.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+    constructor(private prisma: PrismaService){}
+
+    async allUser(){
+        return await this.prisma.users.findMany()
+    }
+}
