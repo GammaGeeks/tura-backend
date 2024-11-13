b387a98beb1e16315b4c9842a56c43e57e15d0e9
NGOGA Adolphe <adolphengoga@gmail.com>
2024-11-13 08:41:35 +0200

    new branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     expandVariables: true
-  })
-    ,PrismaModule, UserModule]
+  }), PrismaModule]
 })
 export class AppModule {}
