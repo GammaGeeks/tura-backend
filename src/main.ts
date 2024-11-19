8d0a9ff9fba67668e354b39cb4a99e5211c549c5
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2024-11-19 12:25:30 +0200

    Fix CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,23 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = [
+    '*',
+    'http://localhost:3000', // Development
+    'https://your-frontend-domain.com', // Production
+  ];
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // Allow request
+      } else {
+        callback(new Error('Not allowed by CORS')); // Block request
+      }
+    },
+    methods: 'GET,HEAD,POST,PUT,DELETE,OPTIONS',
+    credentials: true, // Allow cookies if needed
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
