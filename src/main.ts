a08981366f8366702787f18a47d96dd4b11f7e29
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2024-11-20 09:10:39 +0200

    [Finishes #188511377] [Finishes #188511372] Connecting redux-toolkit

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // or specify allowed origins like ['https://yourfrontenddomain.com']
+    origin: 'https://main.d5fw14mc1awgo.amplifyapp.com', // specify the frontend domain or '*' (not recommended for production)
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   });
 
   await app.listen(process.env.PORT ?? 3000);
