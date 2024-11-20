a37fae4723e6a0e49e73a93621caac8b3efbcbdc
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2024-11-20 09:30:04 +0200

    [Finishes #188511377] [Finishes #188511372] Connecting redux-toolkit

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ async function bootstrap() {
   app.enableCors({
     origin: 'https://main.d5fw14mc1awgo.amplifyapp.com', // specify the frontend domain or '*' (not recommended for production)
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'xyz'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Access-Control-Allow-Credentials',
+      'xyz',
+    ],
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   });
 
